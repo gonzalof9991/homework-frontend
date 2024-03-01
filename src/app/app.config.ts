d3d8417361d80041b774442712b9787d279ecc73
gonzalo9991r <go.rojasf@duocuc.cl>
2024-03-01 15:05:31 -0300

    feat: Implementing format to input date | change loadForm

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,18 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(),
-    provideNativeDateAdapter()
+    provideNativeDateAdapter(
+      {
+        parse: {
+          dateInput: 'DD/MM/YYYY',
+        },
+        display: {
+          dateInput: 'DD/MM/YYYY',
+          monthYearLabel: 'MMM YYYY',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'MMMM YYYY',
+        },
+      }
+    )
   ]
 };
